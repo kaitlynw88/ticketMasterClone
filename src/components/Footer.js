0f8058ac50c8c3849250986142bbabe88ff11090
KaitlynWickson <kwickson28@gmail.com>
2023-01-18 14:09:55 -0500

    edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
       <div className='footerText'>
-          <p>Kaitlyn Wickson | 2023 | TicketMasterAPI</p>
+          <p>Kaitlyn Wickson | 2022 | TicketMasterAPI</p>
           <a href="https://kaitlynwcodes.ca/">My portfolio</a>
       </div>
   );
